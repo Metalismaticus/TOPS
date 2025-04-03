8be99b44a7383c9a492fbeb1c504e07ca4f0b457
Metalismaticus <4istopodely@gmail.com>
2025-04-03 11:37:37 +0300

    Add files via upload

---
+++ sections.js
@@ -0,0 +1,48 @@
+
+const sections = {
+  links: {
+    text: `Ты новенький на сервере и не знаешь, что делать? В начале используй команду для добавления русского чата и настрой его: <br>
+    <code>/group join russian</code><br>
+    Изменения размера чата для удобства переключения между вкладками: <br>
+    <code>.chatsize 800 400</code><br>
+    Отключаем переключение на главный чат: <br>
+    Отключаем "Автопоказ чата" в Настройки -> Интерфейс<br><br>
+    Затем можно добавиться в самое крупное РУ сообщество на этом сервере: <br>
+    <a href="https://t.me/mylesvintagestory" target="_blank">https://t.me/mylesvintagestory</a><br><br>
+    Также полезные ссылки:<br>
+    Карта: <a href="https://map.tops.vintagestory.at/?x=-1&y=17&zoom=10" target="_blank">ссылка</a><br>
+    Навигатор: <a href="https://herrscher-of-sleeping.github.io/tl-navigator/" target="_blank">ссылка</a>`,
+    img: 'img/links.png'
+  },
+  commands: {
+    text: `Смена класса с перезарядкой в реальные полгода:<br>
+    <code>.charsel</code>`,
+    img: 'img/commands.png'
+  },
+  mods: {
+    text: `Перевод сообщений в чате:<br>
+    <a href="https://mods.vintagestory.at/chattranslator" target="_blank">Chat Translator</a><br><br>
+    Показывает список игроков и их пинг на Tab:<br>
+    <a href="https://mods.vintagestory.at/playerlist" target="_blank">Player List</a><br><br>
+    Добавляет возможность смотреть все приваты игроков (по умолчанию на R англ.):<br>
+    <a href="https://mods.vintagestory.at/claimsradar" target="_blank">Claims Radar</a><br><br>
+    Больше информации, от времени горения угольной ямы до информации у какого торговца что и за сколько продается:<br>
+    <a href="https://mods.vintagestory.at/extrainfo" target="_blank">Extra Info</a>`,
+    img: 'img/mods.png'
+  },
+  cities: {
+    text: `Ты должен решить для себя, что тебе интереснее — проходить игру в одиночку или же найти себе город и развиваться в социуме, используя торговлю на максимум. Если тебе интересно играть с живыми людьми, то вот список живых поселков с РУ игроками. На самом деле их больше, но тут указаны только те, которые оставили свои контакты и указали маршруты.<br><br>
+    <ul>
+      <li>Бронзовый город</li>
+      <li>Желтые высоты</li>
+      <li>Аграба</li>
+      <li>Эребор</li>
+    </ul>`,
+    img: 'img/cities.png'
+  },
+  places: {
+    text: `<code>/waypoint addati gear 524 110 437 false #FF0000 Evangeлисткий храм</code><br>
+           <code>/waypoint addati gear -776 110 17047 false #FF0000 Музей диковин</code>`,
+    img: 'img/places.png'
+  }
+};
